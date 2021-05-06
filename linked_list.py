a2d94ba22ff00e08792e16db8cae90818c22d143
mugo <keithgety@gmail.com>
2021-05-06 22:29:11 +0300

    users descending linked_list

---
+++ linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+        
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+        
+        
+    def print_ll(self):
+        ll_string = ""
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f" {str(node.data)} ->"
+            node = node.next_node
+            
+            
+        ll_string+= " None"
+        print(ll_string)
+        
+        
+        
+ll = LinkedList()
+node4 = Node("data4", None)
+node3 = Node("data3", node4)
+node2 = Node("data2", node3)
+node1 = Node("data1", node2)
+
+
+ll.head = node1
+ll.print_ll()
